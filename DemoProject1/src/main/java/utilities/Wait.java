969774aa38a0d1ad4aa6f46a81358bbe40d75d97
Anvith <Anvith@AnoopAcerLaptop>
2026-03-12 22:07:35 +0530

    Latest

--- DemoProject1/src/main/java/utilities/Wait.java
+++ DemoProject1/src/main/java/utilities/Wait.java
@@ -2,45 +2,30 @@ package utilities;
 
 import java.time.Duration;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Wait {
-	
+
 	public static final int IMPLICITWAIT = 5;
 	public static final int EXPLICITWAIT = 10;
 
 	WebDriver driver;
-	
+
 	public void waitForElementToBeClickable(WebDriver driver, WebElement element) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
 		wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
+
 	public void waitForElementTobeVisible(WebDriver driver, WebElement element) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
 		wait.until(ExpectedConditions.visibilityOf(element));
 	}
-	
-    public Alert waitForAlert() {
-    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
-        return wait.until(ExpectedConditions.alertIsPresent());
-    }
-    public void acceptAlert() {
-        Alert alert = waitForAlert();
-        alert.accept();
-    }
 
-     public void dismissAlert() {
-    	 Alert alert = waitForAlert();
-         alert.accept();
-    }
-     public void sendTextToAlert(String text) {
-         Alert alert = waitForAlert();
-         alert.sendKeys(text);
-         alert.accept();
+	public void waitForAlert(WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.alertIsPresent());
+	}
 }
-}
-
